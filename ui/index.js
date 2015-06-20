3296484e865a380d651a540935b531352d49e598
wouldgo <wouldgo84@gmail.com>
2015-06-20 17:06:45 +0200

    disabled if not all params filled

--- ui/index.js
+++ ui/index.js
@@ -36,6 +36,22 @@
         mainWallet.send(amountToSendValue, accountToSendValue, Number(publicKeyToSendValue));
       }
     }
+    , onInputClick = function onInputClick() {
+
+      var accountToSendValue = accountToSendElement.val()
+        , publicKeyToSendValue = publicKeyToSendElement.val()
+        , amountToSendValue = amountToSendElement.val();
+
+      if (accountToSendValue &&
+        publicKeyToSendValue &&
+        amountToSendValue) {
+
+        sendAssetsButtonElement.removeAttr('disabled');
+      } else if (!sendAssetsButtonElement.is(':disabled')) {
+
+        sendAssetsButtonElement.attr('disabled', 'disabled');
+      }
+    };
 
   NXTWrapper.prototype.getAccount = function getAccount() {
 
@@ -118,105 +134,11 @@
   rivets.bind(appElement, viewModel);
   checkBalanceButtonElement.on('click', onCheckBalanceButtonElementClick);
   sendAssetsButtonElement.on('click', onSendAssetsButtonElementClick);
+  accountToSendElement.on('click', onInputClick);
+  publicKeyToSendElement.on('click', onInputClick);
+  amountToSendElement.on('click', onInputClick);
 
-/*
-// var bitcoin = Bitcoin.init()
-// bitcoin.send
-
-
-// models (defaults)
-var models = {
-  Key: {
-    // nothing
-  }
-}
-
-
-// store - object state storage
-var store  = {
-  keys: []
-}
-
-var walletActions = {
-  send: function(amount, addresses)  {
-    // TODO
-  },
-
-  getAddress: function(cb) {
-    var key = models.Key
-    key.id = store.keys.length
-    fetch('http://localhost:3001/address')
-      .then(function(response) {
-        response.json().then(function(address) {
-          var generatedKey = address
-          key = $.extend(generatedKey, key)
-          key.balance = 'loading...'
-          store.keys.push(key)
-          return cb()
-        })
-      })
-  }
-
-}
-
-
-
-// TODO:
-
-// walletActions.send()
-// walletActions.balance()
-
-walletActions.getAddress(function(){
-
-  // view bind
-  rivets.bind($('.entries'), store.keys[0])
-
-  // load view (presenters)
-  // if (store.keys[0]) {
-  var address = store.keys[0].address
-  console.log(address)
-  store.keys[0].address_blockchain_url = 'https://blockchain.info/address/'+address
-  // }
-
-  store.keys[0].amountFiat = '-'
-})
-
-
-
-
-// rivets.bind($('.entries'), messages)
-
-
-
-$('#app').on('click', '.btn-send', function(evt){
-  // TODO <button class='btn-send' rv-on-click='item.send'>Send</button>
-
-  var amount = 0.0001 // BTC // 1000 // satoshi
-
-  var addresses = []
-
-  var address = document.querySelector('input[name=address_to]').value
-
-  addresses.push(address)
-
-  mainWallet.send(amount, addresses) // gogogo! TODO Callback
-})
-
-
-$('#app').on('click', '.balance_check', function(evt){
-  Bitcoin.balance(function(balance){
-    console.log('balance: ', balance)
-    store.keys[0].balance     = balance
-    store.keys[0].balance_btc = balance*Math.pow(10, -8)
-  })
-})
-
-var updateAmountFiat = function(evt) {
-  console.log('TODO: update amount fiat')
-  store.keys[0].amountFiat = 'update'
-}
-
-$('#app').on('change', 'input[name=address_to]', updateAmountFiat)
-$('#app').on('change', 'input[name=currency_to]', updateAmountFiat)
-*/
+  accountToSendElement.on('blur', onInputClick);
+  publicKeyToSendElement.on('blur', onInputClick);
+  amountToSendElement.on('blur', onInputClick);
 }(rivets, window));
